d624c44d927d29a7249743d6a728ff106558ddb8
Edward Lee <edward.lee6128@gmail.com>
2026-02-03 12:14:56 +0100

    created initial models

--- texcably_app/models.py
+++ texcably_app/models.py
@@ -1,3 +1,19 @@
+from django.conf import settings
 from django.db import models
 
+User = settings.AUTH_USER_MODEL
+
 # Create your models here.
+class Word(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='words', db_index=True)
+    primary_language = models.CharField(max_length=100)
+    foreign_language = models.CharField(max_length=100)
+    example_sentence = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Article(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='articles', db_index=True)
+    link = models.URLField(max_length=200)
+    words = models.ManyToManyField(Word, related_name='articles')
+    created_at = models.DateTimeField(auto_now_add=True)
